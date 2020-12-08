92ca569b5573e0cc758f64af2f58a8abd0c6b08f
Adrienne Tacke <adrienne.tacke@gmail.com>
2020-12-07 23:11:16 -0800

    chore: clean up code :hammer:

--- day-8/one.js
+++ day-8/one.js
@@ -1,37 +1,30 @@
 const one = (input) => {
   const instructions = input.split('\n');
 
-  let notAnInfiniteLoop = true;
   let accumulator = 0;
   let currentIndex = 0;
   let stepsVisited = [];
 
-  while (notAnInfiniteLoop) {
-    const [, operation, direction, units] = /(acc|jmp|nop)\s(\+|-)(\d*)/.exec(instructions[currentIndex]);
+  while (!stepsVisited.includes(currentIndex)) {
+    stepsVisited.push(currentIndex);
+
+    const [, operation, units] = /(acc|jmp|nop)\s([+-]\d*)/.exec(instructions[currentIndex]);
 
     switch (operation) {
       case 'acc':
-        if (direction === '+') accumulator += parseInt(units);
-        if (direction === '-') accumulator -= parseInt(units);
+        accumulator += parseInt(units);
         currentIndex++;
 
         break;
 
       case 'jmp':
-        if (direction === '+') currentIndex += parseInt(units);
-        if (direction === '-') currentIndex -= parseInt(units);
+        currentIndex += parseInt(units);
 
         break;
 
       default:
         currentIndex++;
         break;
-    }
-
-    if (stepsVisited.includes(currentIndex)) {
-      notAnInfiniteLoop = false;
-    } else {
-      stepsVisited.push(currentIndex);
     }
   }
 
